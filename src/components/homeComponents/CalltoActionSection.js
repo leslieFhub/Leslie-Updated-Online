a35fbb66efa6a10e6aed3bc6a849ebba75c32267
leslieFhub <134671364+leslieFhub@users.noreply.github.com>
2023-06-06 12:17:58 +0800

    Update CalltoActionSection.js

--- src/components/homeComponents/CalltoActionSection.js
+++ src/components/homeComponents/CalltoActionSection.js
@@ -6,14 +6,6 @@ const CalltoActionSection = () => {
       <div className="container">
         <div className="row">
           <div className="col-xs-12">
-            <div className="subscribe-head">
-              <h2>DO you need more tips?</h2>
-              <p>Sign up free and get the latest tips.</p>
-              <form className="form-section">
-                <input placeholder="Your Email..." name="email" type="email" />
-                <input value="Yes. I want!" name="subscribe" type="submit" />
-              </form>
-            </div>
           </div>
         </div>
       </div>
